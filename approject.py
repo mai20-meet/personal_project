1af7c9d215b4d9ae30f7b509a37361ec3fe72834
myname21-meet <myname21@meet.mit.edu>
2020-01-19 08:44:52 -0500

    pero

--- approject.py
+++ approject.py
@@ -41,9 +41,9 @@ def access():
 def reserches():
 	return render_template("reserches.html")
 
-@app.route('/home')
-def home():
-	return render_template("home.html")
+# @app.route('/home')
+# def home():
+# 	return render_template("home.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
